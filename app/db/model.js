84bc421f75c373841714003d97007579d59fb282
alec-host <irualec@yahoo.com>
2022-05-30 16:57:14 +0300

    3rd commit - store a book request via model.js

--- app/db/model.js
+++ app/db/model.js
@@ -7,13 +7,37 @@ function saveParcelBookingRequest(pool,content,recordCallback) {
 		if(err){
 			throw err;
 		}else{
-			var insert_value = [content.reference_no,content.recipient_name,content.recipient_mobile,content.recipient_addresss,content.pick_up,content.pick_up_address,content.dropp_off,content.dropp_off_address,content.provider,content.date_created];
+			var insert_value = [content.reference_no,content.recipient_name,content.recipient_mobile,content.recipient_addresss,content.pick_up_latlng,content.pick_up_address,content.drop_off,content.drop_off_latlng,content.provider,content.date_created];
 			conn.query("INSERT " + 
 			           "INTO " +
 					   "`db_parcel_service`.`tbl_booking_request` " +
-					   "(`reference_no`,`recipient_name`,`recipient_mobile`,`recipient_addresss`,`pick_up`,`pick_up_address`,`dropp_off`,`dropp_off_address`,`provider`,`date_created`) " +
+					   "(`reference_no`,`recipient_name`,`recipient_mobile`,`recipient_addresss`,`pick_up`,`pick_up_address`,`drop_off`,`drop_off_address`,`provider`,`date_created`) " +
 					   "VALUES (?,?,?,?,?,?,?,?,?,?) " +
 					   "ON DUPLICATE KEY UPDATE `date_modified` = NOW();" ,insert_value, (err, res) => {
+				if(err) throw err;
+				recordCallback(err, res);
+				conn.release();
+			});
+		}
+	});
+}
+
+/**
+ *-.method:updateParcelBookingRequestWithTripID.
+ *-.updates booking request.
+ **/
+function updateParcelBookingRequestWithTripID(pool,tripID,recordCallback) {
+	pool.getConnection((err, conn) => {
+		if(err){
+			throw err;
+		}else{
+			var insert_value = [content.reference_no,content.recipient_name,content.recipient_mobile,content.recipient_addresss,content.pick_up_latlng,content.pick_up_address,content.drop_off,content.drop_off_latlng,content.provider,content.date_created];
+			conn.query("UPDATE " + 
+					   "`db_parcel_service`.`tbl_booking_request` " +
+					   "SET "+
+					   +"`trip_id` = ? " +
+					   "WHERE " +
+					   "`recipient_mobile` = ?;" ,insert_value, (err, res) => {
 				if(err) throw err;
 				recordCallback(err, res);
 				conn.release();
