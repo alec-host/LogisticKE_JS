aacffba217529bb5d766cdfa7a3681817c8cedfa
alec-host <irualec@yahoo.com>
2022-06-17 17:28:42 +0300

    24th commit - installed mysql2 package.

--- app/routes/appRoutes.js
+++ app/routes/appRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql   = require('mysql');
+const mysql   = require('mysql2');
 const crypto  = require('crypto'); 
 
 const db      = require('../db/config/dbConfig');
